5eb1530e572f8d859c48e4f71666e03567f472fc
lilylindstrand <lily@hepno.dev>
2025-02-09 16:13:54 +0100

    init

---
+++ src/main/java/dev/hepno/platinum_game_server/PlatinumGameServerApplication.java
@@ -0,0 +1,13 @@
+package dev.hepno.platinum_game_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatinumGameServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatinumGameServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/hepno/platinum_game_server/PlatinumGameServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hepno.platinum_game_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatinumGameServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
