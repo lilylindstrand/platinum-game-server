1f7416fbbc0438aeb766e12b8ab62825c6f19850
Lily Lindstrand <lily@hepno.dev>
2025-02-23 14:54:58 +0100

    feat(session): setup redis & springsession

--- src/main/java/dev/hepno/platinum_game_server/config/SecurityConfig.java
+++ src/main/java/dev/hepno/platinum_game_server/config/SecurityConfig.java
@@ -3,10 +3,12 @@ package dev.hepno.platinum_game_server.config;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
